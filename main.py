09aa058828c7bba2468d11c2a46503306131216f
Janco <janco.spies@gmail.com>
2023-01-28 15:19:28 +0200

    Added menu to main window

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import tkinter as tk
+from tkinter import *
 import gettext
 
 lang_changed = False
 root = None
 
 translation = gettext.translation(
-        'auction', localedir='translations', languages=['en'])
+    'auction', localedir='translations', languages=['en'])
 translation.install()
 # _ = translation.gettext
 
@@ -19,8 +20,18 @@ def setup_window():
     root.title(_("title"))
     root.geometry("400x400")
 
-    btn_language = tk.Button(root, text=_("lang_change_btn"), command=ask_language)
-    btn_language.pack()
+    frame = Frame(root)
+    frame.pack()
+
+    mainmenu = Menu(frame)
+    mainmenu.add_command(label=_("lang_change_btn"), command=ask_language)
+    mainmenu.add_command(label=_("exit"), command=root.destroy)
+
+    root.config(menu=mainmenu)
+
+    # btn_language = tk.Button(root, text=_(
+    #     "lang_change_btn"), command=ask_language)
+    # btn_language.pack()
 
     lbl_test = tk.Label(root, text=_("test_str"))
     lbl_test.pack()
@@ -41,19 +52,27 @@ def set_language(input_lang):
 def ask_language():
     popup = tk.Toplevel()
     popup.title(translation.gettext("title"))
-    lbl_instruction = tk.Label(popup, text=translation.gettext("lang_change_instr"))
+    popup.geometry("150x150")
+    lbl_instruction = tk.Label(
+        popup, text=translation.gettext("lang_change_instr"))
     lbl_instruction.pack()
-    btn_afrikaans = tk.Button(popup, text="Afrikaans", command=lambda:[set_language("af")])
+    # new_lang = tk.StringVar()
+    # rbtn_afrikaans = tk.Radiobutton(popup, text="Afrikaans", variable=new_lang, value="af", command=set_language(new_lang)).pack()
+    # rbtn_english = tk.Radiobutton(popup, text="English", variable=new_lang, value="en", command=set_language(new_lang)).pack()
+    btn_afrikaans = tk.Button(popup, text="Afrikaans",
+                              command=lambda: [set_language("af")])
     btn_afrikaans.pack()
-    btn_english = tk.Button(popup, text="English", command=lambda:[set_language("en")])
+    btn_english = tk.Button(popup, text="English",
+                            command=lambda: [set_language("en")])
     btn_english.pack()
-    btn_close = tk.Button(popup, text=translation.gettext("close"), command=popup.destroy)
+    btn_close = tk.Button(popup, text=translation.gettext(
+        "close"), command=popup.destroy)
     btn_close.pack()
 
 
 def main():
-    setup_window() 
-    global root 
+    setup_window()
+    global root
 
     root.mainloop()
 
